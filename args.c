90cf8d28d484f41f22584b70d2988c958b93c21d
obed101 <obedamoako92@gmail.com>
2021-08-14 06:35:19 -0700

    Added A 'C' Perfect Calendar

---
+++ args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * main - function (entry point)
+ * @ac: number of items in av
+ * @av: a NULL terminated array of strings
+ */
+int main(int __attribute__((unused)) ac, char **av)
+{
+
+	return (av);
+}
