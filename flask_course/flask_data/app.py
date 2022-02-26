13802edaf465effdadf282ea277d816db6b7a488
Obed101 <obedamoako92@gmail.com>
2022-02-26 09:39:21 +0000

    database query testing

--- flask_course/flask_data/app.py
+++ flask_course/flask_data/app.py
@@ -4,9 +4,12 @@ This module is for my flask app
 Implementing the studies from flask course
 """
 from flask import Flask
+from faker import Faker
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import random
 
+fake = Faker()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -42,9 +45,88 @@ class Order(db.Model):
 
     products = db.relationship('Product', secondary='order_product')
 
+
 class Product(db.Model):
     """The Product class"""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(35), nullable=False, unique=True)
     price = db.Column(db.Integer, nullable=False)
 
+
+def add_customers():
+    for _ in range(100):
+        customer = Customer(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            address=fake.street_address(),
+            city=fake.city(),
+            postcode=fake.postcode(),
+            email=fake.email()
+        )
+        db.session.add(customer)
+    db.session.commit()
+
+def add_orders():
+    customers = Customer.query.all()
+
+    for _ in range(1000):
+        #choose a random customer
+        customer = random.choice(customers)
+
+        ordered_date = fake.date_time_this_year()
+        shipped_date = random.choices([None, fake.date_time_between(start_date=ordered_date)], [10, 90])[0]
+
+        #choose either random None or random date for delivered and shipped
+        delivered_date = None
+        if shipped_date:
+            delivered_date = random.choices([None, fake.date_time_between(start_date=shipped_date)], [50, 50])[0]
+
+        #choose either random None or one of three coupon codes
+        coupon_code = random.choices([None, '50OFF', 'FREESHIPPING', 'BUYONEGETONE'], [80, 5, 5, 5])[0]
+
+        order = Order(
+            customer_id=customer.id,
+            order_date=ordered_date,
+            shipped_date=shipped_date,
+            delivered_date=delivered_date,
+            coupon_code=coupon_code
+        )
+
+        db.session.add(order)
+    db.session.commit()
+
+def add_products():
+    for _ in range(10):
+        product = Product(
+            name=fake.color_name(),
+            price=random.randint(10,100)
+        )
+        db.session.add(product)
+    db.session.commit()
+
+def add_order_products():
+    orders = Order.query.all()
+    products = Product.query.all()
+
+    for order in orders:
+        #select random k
+        k = random.randint(1, 3)
+        # select random products
+        purchased_products = random.sample(products, k)
+        order.products.extend(purchased_products)
+
+    db.session.commit()
+
+def create_random_data():
+    db.create_all()
+    add_customers()
+    add_orders()
+    add_products()
+    add_order_products()
+
+def get_orders_by(customer_id=1):
+    """Gets all orders a customer has made"""
+    customer_orders = Order.query.filter_by(customer_id=customer_id).all()
+    print(f"Here are the orders made by {customer_orders[1].customer.first_name}")
+    for order in customer_orders:
+        print(order.id)
